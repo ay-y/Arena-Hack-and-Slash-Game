57a6b6e9f3bcea618c6d31d5da16fe4fe732c386
ay-y <31833973+ay-y@users.noreply.github.com>
2017-10-25 22:08:06 -0400

    commit unity file and model

---
+++ Hack and Slash Arena Game/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+
+    private Rigidbody rigidBody;
+    public float accel = 1000;
+    public float maxVelocity = 1000;
+
+    // Use this for initialization
+    void Start () {
+        rigidBody = GetComponent<Rigidbody>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void FixedUpdate()
+    {
+        rigidBody.AddForce(new Vector3(Input.GetAxis("Horizontal") * Time.deltaTime * accel, 0, Input.GetAxis("Vertical") * Time.deltaTime * accel));
+
+        transform.position = new Vector3(transform.position.x, 0, transform.position.z);
+        rigidBody.velocity = Vector3.ClampMagnitude(rigidBody.velocity, maxVelocity);
+    }
+}
