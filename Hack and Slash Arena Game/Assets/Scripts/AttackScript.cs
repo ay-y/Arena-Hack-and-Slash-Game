8a3b68584481288d9cae02d3a2ccd7cad6beb74a
Joe <jhumes742@gmail.com>
2017-11-15 19:29:57 -0500

    Implemented lots of weapon stuff
    Create a script for attacks that in the future will let me customize attacks. Also created 2 new attacks, they should all behave the same right now though. Implemented a function to switch between weapons. Messed with controls

---
+++ Hack and Slash Arena Game/Assets/Scripts/AttackScript.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackScript : MonoBehaviour {
+
+    private float coolDown;
+    private float speed;
+    private float destroyTime;
+    private float damage;
+
+	// Use this for initialization
+	void Start () {
+        coolDown = 1.0f;
+        speed = 60f;
+        destroyTime = 2.0f;
+        damage = 25f;
+	}
+
+    public float getCooldown()
+    {
+        return coolDown;
+    }
+
+    public float getSpeed()
+    {
+        return speed;
+    }
+
+    public float getDestroyTime()
+    {
+        return destroyTime;
+    }
+
+    public float getDamage()
+    {
+        return damage;
+    }
+}
