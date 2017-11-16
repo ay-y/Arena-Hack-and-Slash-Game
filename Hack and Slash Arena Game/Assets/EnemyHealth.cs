e598b5d3260b5b0b843df1a94c6e1b387f210294
Arlunn <32222430+Arlunn@users.noreply.github.com>
2017-11-15 22:04:29 -0500

    wah

---
+++ Hack and Slash Arena Game/Assets/EnemyHealth.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHealth : MonoBehaviour {
+
+    public int startingHealth;
+    public int currentHealth;
+    bool isDead;
+    // Use this for initialization
+    void Start()
+    {
+        currentHealth = startingHealth;
+    }
+
+    public void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+        if (currentHealth <= 0 && !isDead)
+        {
+            Death();
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "PlayerProjectile")
+        {
+            TakeDamage(35);
+        }
+    }
+
+    private void Death()
+    {
+        Destroy(gameObject);
+
+    }
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
