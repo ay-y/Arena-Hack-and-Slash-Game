d9dbb0bd4d33f810c0ba58b66cf08d0368b1dafe
Joe <jhumes742@gmail.com>
2017-11-19 22:29:59 -0500

    bullcrap
    hey guess what return values don't work so buckle up boys we're using CONSTANTS YEEEEEEE HAAAWWWWWWW

--- Hack and Slash Arena Game/Assets/Scripts/PlayerController.cs
+++ Hack and Slash Arena Game/Assets/Scripts/PlayerController.cs
@@ -288,13 +288,13 @@ public class PlayerController : MonoBehaviour {
         }
 
 		// Add velocity to the bullet
-		attack.GetComponent<Rigidbody>().velocity = attack.transform.forward * Primary.getSpeed();
+		attack.GetComponent<Rigidbody>().velocity = attack.transform.forward * 60.0f;
 
 		// Destroy the bullet after specified amount of time
-		Destroy(attack, Primary.getDestroyTime());
+		Destroy(attack, 2.0f);
 
         //Primary cooldown 
-        primaryCooldown = Primary.getCooldown();
+        primaryCooldown = 1.0f;
 	}
 
     private void SecondaryAttack()
@@ -319,13 +319,13 @@ public class PlayerController : MonoBehaviour {
         }
 
         // Add velocity to the bullet
-        attack.GetComponent<Rigidbody>().velocity = attack.transform.forward * Secondary.getSpeed();
+        attack.GetComponent<Rigidbody>().velocity = attack.transform.forward * 80.0f;
 
         // Destroy the bullet after specified amount of time
-        Destroy(attack, Secondary.getDestroyTime());
+        Destroy(attack, 1.5f);
 
         //secondary cooldown 
-        secondaryCooldown = Secondary.getCooldown();
+        secondaryCooldown = 1.0f;
     }
 
 }
