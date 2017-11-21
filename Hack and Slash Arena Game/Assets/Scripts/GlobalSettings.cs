110def7da926f3c3969b9c332ee0d2b5d257134e
ay-y <31833973+ay-y@users.noreply.github.com>
2017-11-20 23:29:52 -0500

    added spawn point system
    - in markScene2, press "m" to spawn an enemy at a random spawn point

--- Hack and Slash Arena Game/Assets/Scripts/GlobalSettings.cs
+++ Hack and Slash Arena Game/Assets/Scripts/GlobalSettings.cs
@@ -8,24 +8,39 @@ public class GlobalSettings : MonoBehaviour {
     public bool keyboard_activated;
     private Text timerText;
     private float timer = 0.0f;
+    private int wave = 0;
+    private GameObject[] players;
+    private GameObject[] enemies;
+    private GameObject[] spawnPoints;
+    public GameObject virusEnemy;
 
     // Use this for initialization
 	void Start () {
         keyboard_activated = false;
         Cursor.lockState = CursorLockMode.Confined;
         //timerText = GameObject.Find("TimerText").GetComponent<Text>();
-        timer = 100.0f;
+        timer = 10.0f;
+        players = GameObject.FindGameObjectsWithTag("Player");
+        spawnPoints = GameObject.FindGameObjectsWithTag("Spawn");
     }
 	
 	// Update is called once per frame
 	void Update () {
+
+        enemies = GameObject.FindGameObjectsWithTag("Enemy");
+
 
         // press k to activate/deactivate keyboard input
         // will be a menu setting rather than a key press in the final version
         if (Input.GetKeyDown("k"))
         { keyboard_activated = !keyboard_activated; }
 
-
+        if (Input.GetKeyDown("m") && enemies.Length < 41)
+        {
+            int randomSpawn = Random.Range(0, spawnPoints.Length);
+            Debug.Log(randomSpawn);
+            spawnPoints[randomSpawn].GetComponent<SpawnScript>().SpawnEnemies(1, virusEnemy);
+        }
 
         // wave timer
         //timer = timer - Time.deltaTime;
@@ -34,5 +49,5 @@ public class GlobalSettings : MonoBehaviour {
 
         // enemy counter
 
-	}
+    }
 }
---
+++ Hack and Slash Arena Game/Assets/Scripts/SpawnScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnScript : MonoBehaviour {
+
+    private int spawnCount = 0;
+    private GameObject spawnType;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (spawnCount > 0)
+        {
+            GameObject enemy = (GameObject)Instantiate(
+            spawnType,
+            new Vector3(transform.position.x + Random.Range(-5.0f, 5.0f), 0, transform.position.z + Random.Range(-5.0f, 5.0f)),
+            transform.rotation);
+
+            spawnCount = spawnCount - 1;
+        }
+	}
+
+    public void SpawnEnemies(int numEnemies, GameObject typeEnemy)
+    {
+        spawnCount = numEnemies;
+        spawnType = typeEnemy;
+    }
+}
